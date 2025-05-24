8374102e013de9b4a5da120274e7f0e9b23b6d14
JordyEstaTrabajando <jordyisworking@gmail.com>
2025-05-24 18:19:46 -0500

    Subida4

--- js/main-js.js
+++ js/main-js.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("nav-content-btn");
-    const nav = document.getElementById("nav-content-menu");
+const btn = document.getElementById("nav-content-btn");
+const nav = document.getElementById("nav-content-menu");
 
-    btn.addEventListener("click", function () {
-        nav.classList.toggle("menu-visible");
-    });
+btn.addEventListener("click", function () {
+    if (nav.style.display === "none") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+    
 });
